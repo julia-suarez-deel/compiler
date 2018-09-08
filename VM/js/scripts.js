6c1d5c2420d35822c7d4c99afe103c5d5a9552d9
Julia Suarez <julia.suarez@unet.edu.ve>
2018-09-08 02:04:39 -0400

    :snowman: Add source code loader - Include JQuery library - Add functions for loading source code and execute a function per each instruction, and pass the arguments to it.

---
+++ VM/js/scripts.js
@@ -0,0 +1,46 @@
+const CODE_URL = 'http://localhost:63342/compiler/VM/code.txt';
+$(document).ready(function(){
+    readCode();
+});
+function readCode(){
+    $.get(CODE_URL, function(data) {
+        let instructions = splitInstructions(data);
+        instructions.forEach(line => {
+            try{
+                let instruction = splitArguments(line);
+                instruction.function = window[instruction.name];
+                if(instruction.function.length === instruction.args.length){
+                    instruction.function.apply(this, instruction.args);
+                }
+                else{
+                    throw new Error("Number of arguments doesn't match.")
+                }
+            }catch (e) {
+                console.log(e.message);
+            }
+        });
+     }, 'text');
+    console.log('end');
+}
+function splitInstructions(data){
+    let clean_data = data.replace(/[\r\n]+/g,'');
+    let instructions = clean_data.split(';');
+    let instructions_not_empty = instructions.filter( x => x);
+    return instructions_not_empty;
+}
+function splitArguments(data) {
+    let instruction={};
+    instruction.name = data.split(" ")[0];
+    instruction.args = data.split(" ");
+    delete instruction.args[0];
+    return instruction;
+}
+function instruction1(self,a,b,c){
+    console.log('instruction1');
+}
+function instruction2(a,b,c){
+    console.log('instruction2');
+}
+function instruction3(b,c){
+    console.log('instruction3');
+}
