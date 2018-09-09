f2f13784d9dbf3053210942aaf38420efd91f8be
Julia Suarez <julia.suarez@unet.edu.ve>
2018-09-08 21:36:51 -0400

    :snowman: Feature: Toolbar for executing instructions - Add mustache.js library for defining templates like .blade.php for injecting data without having to modify the DOM manually with JQuery.
    - Separate ISA functions in ISA.js
    
    - Add a file helpers.js for extra functions
    
    - Create a template.html file for declare html elements which will be
    injected later in scripts.js with data.
    
    - Create a class for Instruction with:
     name,
     arguments,
     line (whole instruction),
     number,
     function => correspondent function on ISA array,
     $node => correspondent jquery node of correspondent DOM element "tr.instruction",
     parent_selector => string correspondent to selector of the parent of
    the instruction element,
     template_selector => for identify the template
    element in template.html
     unique_selector => for finding the instruction element easily in the DOM
     in case needed.
    
     - Create Toolbox class for storing data of the buttons that compose
     the toolbox, with methods for managing the state of the buttons.
    
     - Create a method for render objects "createOrUpdateDomElement" which
     takes an object with attr, and a variable create to choose if it should
     create the element or update it, it use Mustache.js for loading the
     template and inject the object data, then depending the case, append the new element to the DOM, or updates the existing element.

---
+++ VM/js/ISA.js
@@ -0,0 +1,9 @@
+function instruction1(a, b, c){
+    console.log('Llamada a: instruction1('+a+','+b+','+c+')');
+}
+function instruction2(b, c){
+    console.log('Llamada a: instruction2('+b+','+c+')');
+}
+function instruction3(b, c){
+    console.log('Llamada a: instruction3('+b+','+c+')');
+}
--- VM/js/scripts.js
+++ VM/js/scripts.js
@@ -1,50 +1,144 @@
+// Variables
 const CODE_URL = 'http://localhost:63342/compiler/VM/code.txt';
+const TEMPLATES_URL = 'http://localhost:63342/compiler/VM/templates.html';
 const ISA = {
     'instruction1':instruction1,
     'instruction2':instruction2,
     'instruction3':instruction3,
 };
-$(document).ready(function(){
-    readCode();
-});
-function readCode(){
-    $.get(CODE_URL, function(data) {
-        let instructions = splitInstructions(data);
-        instructions.forEach(line => {
-            try{
-                let instruction = splitArguments(line);
-                instruction.function = ISA[instruction.name];
-                if(instruction.function.length === instruction.args.length){
-                    instruction.function.apply(this, instruction.args);
-                }
-                else{
-                    throw new Error("Number of arguments doesn't match.")
-                }
-            }catch (e) {
-                console.log(e.message);
+const SUCCESS_ROW_CLASS = 'bg-success text-light';
+let is_executing = false;
+let PC = 0;
+let $templates = null;
+let instructions = [];
+let toolbar = null;
+// Helper classes
+class Toolbar {
+    constructor() {
+        this.$node = null;
+        this.unique_selector = '#toolbar';
+        this.template_selector = '#toolbar';
+        this.parent_selector = '#toolbar-container';
+        this.start_or_next = { icon:'play' };
+        this.stop = this.redo = { state: 'disabled' };
+        createOrUpdateDomNode(this, true);
+        this.setupEvents();
+    }
+    updateState(){
+        if(is_executing){
+            this.start_or_next.icon = 'arrow-right';
+            this.stop.state = this.redo.state = '';
+        }
+        createOrUpdateDomNode(this);
+        this.setupEvents();
+    }
+    setupEvents(){
+        $('#start_or_next.btn').on('click', function () {
+            if(PC+1<=instructions.length){
+                is_executing = true;
+                toolbar.updateState();
+                instructions[PC].execute();
             }
+            else{
+                haltProgram();
+            }
+        });
+        $('#stop.btn').on('click', function () {
+            if(is_executing){
+                is_executing = false;
+                PC = 0;
+                toolbar.updateState();
+                haltProgram();
+            }
+        });
+        $('#redo.btn').on('click', function () {
+            if(is_executing){
+                PC = 0;
+                instructions[PC].execute();
+            }
+        })
+    }
+}
+class Instruction{
+    constructor(line, i){
+        let tokens = line.split(" ");
+        let instruction_name = tokens[0];
+        tokens.splice(0, 1);
+        this.parent_selector = '#instructions-container';
+        this.template_selector ='.instruction';
+        this.unique_selector = '.instruction[number='+i+']';
+        this.name =  instruction_name;
+        this.args = tokens;
+        this.line = line;
+        this.number = i;
+        this.function = ISA[instruction_name];
+        this.$node = null;
+    }
+    execute(){
+        $('.instruction').removeClass(SUCCESS_ROW_CLASS);
+        try{
+            if(this.function.length === this.args.length){
+                this.function.apply(this, this.args);
+                this.$node.addClass(SUCCESS_ROW_CLASS);
+                PC++;
+            }
+            else{
+                throw new Error("Number of arguments doesn't match.")
+            }
+        }catch (e) {
+            console.log(e.message);
+        }
+    }
+}
+// Setup
+$(document).ready(function(){
+    loadTemplates();
+    readCode();
+    toolbar = new Toolbar();
+});
+// Helper functions
+function loadTemplates(){
+    $.ajax({
+        url:TEMPLATES_URL,
+        async:false,
+        complete:function(response) {
+            let clean_template = cleanLine(response.responseText);
+            $templates = $(clean_template);
+        }
+    });
+}
+function readCode(){
+    $.get(CODE_URL, function(program) {
+        let program_lines = splitInstructions(program);
+        let i=1;
+        program_lines.forEach(line => {
+            let instruction = new Instruction(line, i);
+            instructions.push(instruction);
+            createOrUpdateDomNode(instruction, true);
+            i++;
         });
      }, 'text');
 }
 function splitInstructions(data){
-    let clean_data = data.replace(/[\r\n]+/g,'');
+    let clean_data = cleanLine(data);
     let instructions = clean_data.split(';');
     let instructions_not_empty = instructions.filter( x => x);
     return instructions_not_empty;
 }
-function splitArguments(data) {
-    let instruction={};
-    instruction.name = data.split(" ")[0];
-    instruction.args = data.split(" ");
-    instruction.args.splice(0,1);
-    return instruction;
+function createOrUpdateDomNode(object, create=false){
+    let template_html = $templates.find(object.template_selector)[0].outerHTML;
+    let rendered_html = Mustache.render(template_html, object);
+    if(create){
+        $(object.parent_selector).append(rendered_html);
+        object.$node = $(object.unique_selector);
+    }
+    else{
+        $(object.unique_selector).html(rendered_html);
+    }
 }
-function instruction1(a,b,c){
-    console.log('instruction1');
-}
-function instruction2(a,b,c){
-    console.log('instruction2');
-}
-function instruction3(b,c){
-    console.log('instruction3');
+function haltProgram() {
+    PC = 0;
+    $('.instruction').removeClass(SUCCESS_ROW_CLASS);
+    $('#toolbar').remove();
+    toolbar = new Toolbar();
 }
